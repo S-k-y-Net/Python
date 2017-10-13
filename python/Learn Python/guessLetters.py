a171bd4423f1ea2bc2e09cd03ea91ad9ab0e6897
Rasulzoda <RasulzodaM@terralink.ru>
2017-10-13 18:57:35 +0300

    Hangman game finished

--- python/Learn Python/guessLetters.py
+++ python/Learn Python/guessLetters.py
@@ -12,25 +12,48 @@ def pick_word():
 		picks = content[round(random.random()*int(len(content)))] 
 		return picks
 
-def guess_word():
+def hangman():
 	c = pick_word() 
+	tryes = 0
+	i = 0
+	user_input_incorrect = []
 	#print(c)
 	user_input = input("Guess a word=) : ")
 	_str = list("_"*len(c))
 	while True:
-		if user_input.upper() in c:
+		tryes += 1
+		if user_input.upper() in c and not user_input.upper() in _str:
+			tryes = 0
 			for i, alf in enumerate(c):
 				if alf == user_input.upper():
 					_str[i] = alf
 			print(" ".join(_str))
 			if _str == list(c):
+				print("Yes is " + " ".join(_str))
 				break
 			user_input = input("... : ")
 		else:
-			user_input = input("Incorrect ^^ : ")
+			if tryes == 6:
+				print("You lose! T_T")
+				break
+			user_input_incorrect.append(user_input)
+			i += 1
+			user_input = input("Incorrect ^^ You have only " + str(6 - tryes) + " tryes \n")
+			if user_input in user_input_incorrect:
+				tryes -= 1
+
+	print("Do you want try again? (Y,N) ")
+	user_answer = input()
+	if user_answer.upper() == "Y":
+		hangman()
+	else:
+		print("GoodBay")
 	return " ".join(_str)
+
+
+
 if __name__ == '__main__':
 	# print(user_input.upper())
 	# print(list(c))
-	c = guess_word() 
-	print("Yes is " + str(c))
+	c = hangman() 
+	
