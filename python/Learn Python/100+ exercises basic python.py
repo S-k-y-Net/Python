2f90a783a730ce298b647113ac2acd8ad1a47b6f
Rasulzoda <RasulzodaM@terralink.ru>
2017-10-17 19:26:53 +0300

    increase 100+ exercises

--- python/Learn Python/100+ exercises basic python.py
+++ python/Learn Python/100+ exercises basic python.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import datetime
+import calendar
+
 
 def GetPythonVersion():
 	version = os.system("Python -V")
@@ -19,8 +21,8 @@ def AreaByRadius(radius):
 #print(AreaByRadius(5))
 
 def FirstlastName():
-	user_input_surname = input("Print you first name: ")
-	user_input_name = input("Print you last name: ")
+	user_input_surname = input("Print your first name: ")
+	user_input_name = input("Print your last name: ")
 	print(user_input_name[::-1])
 	print(user_input_surname[::-1])
 #FirstlastName()
@@ -31,11 +33,48 @@ def ListAndTuple():
 	input_tuple = tuple(input_list)
 	return input_list, input_tuple
 #print(ListAndTuple())
+
 def ExtentionOfFile():
 	user_input = input("input filname with extention: ")
 	try:
 		user_input = user_input.split(".")
-		print(user_input[:0:-1])
+		print(user_input[:len(user_input) - 2:-1])
 	except sys.stderr:
 		print(sys.stderr)
-ExtentionOfFile()
+#ExtentionOfFile()
+
+def LastAndFirstElemet(_list):
+	if _list == [] or _list == None:
+		print("Error")
+	else:
+		print(_list[0],_list[-1])
+color_list = ["Red","Green","White" ,"Black"]
+#LastAndFirstElemet(color_list)
+
+def ExaminationShedule(_tuple):
+	print("The examination will start in " + "%i / %i / %i"%_tuple)
+exam_st_date = (11, 12, 2014)
+#ExaminationShedule(exam_st_date)
+
+def ComputeN_NN_NNN(n):
+	print(n + int('%s%s'%(n,n)) + int('%s%s%s'%(n,n,n)))
+#ComputeN_NN_NNN(5)
+
+def FunctionInfo(name):
+	print(name.__doc__)
+#FunctionInfo(abs)
+
+def MonthAndYearCalendar(year, month):
+	print(calendar.month(year,month))
+#MonthAndYearCalendar(2007, 8)
+
+def PrintSomethingBullshit():
+	print('''a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example''')
+#PrintSomethingBullshit()
+
+def DaysBetween(date_one, date_two):
+	print(abs(datetime.date(date_one[0],date_one[1],date_one[2]) - datetime.date(date_two[0],date_two[1],date_two[2])))
+DaysBetween([2015,8,12],[2011,3,2])
